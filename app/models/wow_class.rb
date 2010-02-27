c07ba82eb38e7f5da6108ef741a31a2d7bf4fa52
Nolan Evans <nolanevans@nolan-evanss-macbook-pro.local>
2010-02-27 12:19:12 -0800

    moving wow class attributes into a constant module

--- app/models/wow_class.rb
+++ app/models/wow_class.rb
@@ -2,16 +2,16 @@ class WowClass < ActiveRecord::Base
   has_many :characters
   serialize :stat_multipliers
   
-  module StatMultipliers
-    DeathKnight = {:attack_power => 0.5, :agility => 1, :armor_penetration => 1.1, :crit => 0.75, :haste => 0.7, :hit => 0.8}
-    Druid = {:attack_power => 0.5, :agility => 1, :armor_penetration => 1.1, :crit => 0.75, :haste => 0.7, :hit => 0.8}
-    Hunter = {:attack_power => 0.5, :agility => 1, :armor_penetration => 1.1, :crit => 0.75, :haste => 0.7, :hit => 0.8}
-    Mage = {:attack_power => 0.5, :agility => 1, :armor_penetration => 1.1, :crit => 0.75, :haste => 0.7, :hit => 0.8}
-    Paladin = {:attack_power => 0.5, :agility => 1, :armor_penetration => 1.1, :crit => 0.75, :haste => 0.7, :hit => 0.8}
-    Priest = {:attack_power => 0.5, :agility => 1, :armor_penetration => 1.1, :crit => 0.75, :haste => 0.7, :hit => 0.8}
-    Rogue = {:attack_power => 0.5, :agility => 1, :armor_penetration => 1.1, :crit => 0.75, :haste => 0.7, :hit => 0.8}
-    Shaman = {:attack_power => 0.5, :agility => 1, :armor_penetration => 1.1, :crit => 0.75, :haste => 0.7, :hit => 0.8}
-    Warlock = {:attack_power => 0.5, :agility => 1, :armor_penetration => 1.1, :crit => 0.75, :haste => 0.7, :hit => 0.8}
-    Warrior = {:attack_power => 0.5, :agility => 1, :armor_penetration => 1.1, :crit => 0.75, :haste => 0.7, :hit => 0.8}
+  module WowClassConstants
+    DeathKnight = {:name => "Death Knight", :stat_multipliers => {:attack_power => 0.5, :agility => 1, :armor_penetration => 1.1, :crit => 0.75, :haste => 0.7, :hit => 0.8}}
+    Druid = {:name => "Druid", :stat_multipliers => {:attack_power => 0.5, :agility => 1, :armor_penetration => 1.1, :crit => 0.75, :haste => 0.7, :hit => 0.8}}
+    Hunter = {:name => "Hunter", :stat_multipliers => {:attack_power => 0.5, :agility => 1, :armor_penetration => 1.1, :crit => 0.75, :haste => 0.7, :hit => 0.8}}
+    Mage = {:name => "Mage", :stat_multipliers => {:attack_power => 0.5, :agility => 1, :armor_penetration => 1.1, :crit => 0.75, :haste => 0.7, :hit => 0.8}}
+    Paladin = {:name => "Paladin", :stat_multipliers => {:attack_power => 0.5, :agility => 1, :armor_penetration => 1.1, :crit => 0.75, :haste => 0.7, :hit => 0.8}}
+    Priest = {:name => "Priest", :stat_multipliers => {:attack_power => 0.5, :agility => 1, :armor_penetration => 1.1, :crit => 0.75, :haste => 0.7, :hit => 0.8}}
+    Rogue = {:name => "Rogue", :stat_multipliers => {:attack_power => 0.5, :agility => 1, :armor_penetration => 1.1, :crit => 0.75, :haste => 0.7, :hit => 0.8}}
+    Shaman = {:name => "Shaman", :stat_multipliers => {:attack_power => 0.5, :agility => 1, :armor_penetration => 1.1, :crit => 0.75, :haste => 0.7, :hit => 0.8}}
+    Warlock = {:name => "Warlock", :stat_multipliers => {:attack_power => 0.5, :agility => 1, :armor_penetration => 1.1, :crit => 0.75, :haste => 0.7, :hit => 0.8}}
+    Warrior = {:name => "Warrior", :stat_multipliers => {:attack_power => 0.5, :agility => 1, :armor_penetration => 1.1, :crit => 0.75, :haste => 0.7, :hit => 0.8}}
   end
 end
--- lib/wow_class_importer.rb
+++ lib/wow_class_importer.rb
@@ -2,7 +2,7 @@ class WowClassImporter
   def self.import_all_classes
     class_names = ["Death Knight", "Druid", "Hunter", "Paladin", "Priest", "Rogue", "Shaman", "Warlock", "Warrior"]
     class_names.each do |class_name|
-      WowClass.create!(:name => class_name, :stat_multipliers => "WowClass::StatMultipliers::#{class_name.gsub(/\s/, '')}".constantize)
+      WowClass.create!("WowClass::WowClassConstants::#{class_name.gsub(/\s/, '')}".constantize)
     end
   end
 end
