65a214a0528dad465336f96d5b1cac6f5e4f0b1a
Nolan Evans <nolanevans@nolan-evanss-macbook-pro.local>
2010-03-24 21:06:20 -0700

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'wowr'
 WowClassImporter.import_all_classes
 FromTextFileItemImporter.import!("#{RAILS_ROOT}/db/data/items/hunter.txt")
 FromTextFileItemImporter.import!("#{RAILS_ROOT}/db/data/items/rogue.txt")
