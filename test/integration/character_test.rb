8bff582b4381d8dfc39b6261beec1ce1dbd0ef4e
Nolan Evans <nolanevans@nolan-evanss-macbook-pro.local>
2010-03-28 14:05:36 -0700

    added some should eventually tests to fix when the armory comes back up

--- test/integration/character_test.rb
+++ test/integration/character_test.rb
@@ -9,5 +9,8 @@ class CharacterTest < ActiveSupport::TestCase
         assert_equal "Baelgun", rails.realm
       end
     end
+    
+    should_eventually "re-import/reparse a character if they already exist"
+    should_eventually "display a unable to fetch the latest data for your character, wow armory may be down"
   end
 end
