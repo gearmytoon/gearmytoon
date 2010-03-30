0d3938bc978987db2dfe35264cad773fce0fdd6f
Nolan Evans <nolanevans@nolan-evanss-macbook-pro.local>
2010-03-30 00:02:31 -0700

    cleaning up item importer

--- lib/item_importer.rb
+++ lib/item_importer.rb
@@ -1,45 +1,37 @@
 class ItemImporter
   QUALITY_ADJECTIVE_LOOKUP = {0 => "poor", 1 => "common", 2 => "uncommon", 3 => "rare", 4 => "epic", 5 => "legendary", 6 => "artifact", 7 => "heirloom"}
-  def self.quality(wowarmory_item)
-    QUALITY_ADJECTIVE_LOOKUP[wowarmory_item.quality]
+  RANGED_WEAPONS = ["Bow", "Gun", "Crossbow", "Thrown"]
+
+  attr_reader :wowarmory_item, :wowarmory_item_id
+  def initialize(wowarmory_item, wowarmory_item_id)
+    @wowarmory_item = wowarmory_item
+    @wowarmory_item_id = wowarmory_item_id
   end
   
-  def self.api
-    @@api ||= Wowr::API.new()
-  end
-
-  def self.import_from_wowarmory!(wowarmory_item_id)
-    if Item.find_by_wowarmory_item_id(wowarmory_item_id).nil?
-      begin
-        wowarmory_item = api.get_item(wowarmory_item_id)
-        if wowarmory_item.cost && wowarmory_item.cost.tokens
-          if wowarmory_item.cost.tokens.length == 1 #TODO: determine the cost of items that cost more then one kind of thing
-            source_wowarmory_item_id = wowarmory_item.cost.tokens.first.instance_variable_get(:@id)
-            token_cost = wowarmory_item.cost.tokens.first.count
-          end
-        end
-        source_area = get_dungeon_source(wowarmory_item)
-        armor_type_name = wowarmory_item.equip_data.subclass_name ? wowarmory_item.equip_data.subclass_name : "Miscellaneous"
-        Item.create!(:wowarmory_item_id => wowarmory_item_id, :name => wowarmory_item.name,
-                     :quality => quality(wowarmory_item), :inventory_type => wowarmory_item.equip_data.inventory_type,
-                     :source_wowarmory_item_id => source_wowarmory_item_id, :icon => wowarmory_item.icon, :bonuses => get_item_bonuses(wowarmory_item),
-                     :armor_type => ArmorType.find_or_create_by_name(armor_type_name), :token_cost => token_cost,
-                     :source_area => source_area)
-      rescue Wowr::Exceptions::ItemNotFound => e
-        STDERR.puts e
+  def import!
+    if wowarmory_item.cost && wowarmory_item.cost.tokens
+      if wowarmory_item.cost.tokens.length == 1 #TODO: determine the cost of items that cost more then one kind of thing
+        source_wowarmory_item_id = wowarmory_item.cost.tokens.first.instance_variable_get(:@id)
+        token_cost = wowarmory_item.cost.tokens.first.count
       end
     end
+    source_area = get_dungeon_source
+    armor_type_name = wowarmory_item.equip_data.subclass_name ? wowarmory_item.equip_data.subclass_name : "Miscellaneous"
+    Item.create!(:wowarmory_item_id => wowarmory_item_id, :name => wowarmory_item.name,
+                 :quality => quality, :inventory_type => wowarmory_item.equip_data.inventory_type,
+                 :source_wowarmory_item_id => source_wowarmory_item_id, :icon => wowarmory_item.icon, :bonuses => get_item_bonuses,
+                 :armor_type => ArmorType.find_or_create_by_name(armor_type_name), :token_cost => token_cost,
+                 :source_area => source_area)
   end
 
-  def self.get_dungeon_source(wowarmory_item)
+  def get_dungeon_source
     if wowarmory_item.drop_creatures.try(:first)
       area_id = wowarmory_item.item_source.area_id
       Area.find_or_create_by_wowarmory_area_id_and_difficulty_and_name(area_id, wowarmory_item.item_source.difficulty, wowarmory_item.item_source.area_name)
     end
   end
 
-  RANGED_WEAPONS = ["Bow", "Gun", "Crossbow", "Thrown"]
-  def self.get_item_bonuses(wowarmory_item)
+  def get_item_bonuses
     returning wowarmory_item.bonuses do |bonuses|
       if damage = wowarmory_item.instance_variable_get(:@tooltip).instance_variable_get(:@damage) #wow wtf wowr gem you fucking suck, seriously.
         if RANGED_WEAPONS.include?(wowarmory_item.equip_data.subclass_name)
@@ -54,4 +46,24 @@ class ItemImporter
       end
     end
   end
+  
+  def quality
+    QUALITY_ADJECTIVE_LOOKUP[wowarmory_item.quality]
+  end
+  
+
+  def self.api
+    @@api ||= Wowr::API.new()
+  end
+  def self.import_from_wowarmory!(wowarmory_item_id)
+    if Item.find_by_wowarmory_item_id(wowarmory_item_id).nil?
+      begin
+        wowarmory_item = api.get_item(wowarmory_item_id)
+        ItemImporter.new(wowarmory_item, wowarmory_item_id).import!
+      rescue Wowr::Exceptions::ItemNotFound => e
+        STDERR.puts e
+      end
+    end
+  end
+
 end
