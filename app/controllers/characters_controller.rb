46283f7785dfec93ec8a1d9a8772b1d432a5ff6d
Adam Anderson <adamandersonis@gmail.com>
2010-04-01 11:02:17 -0700

    Re-enabling CharactersController integration tests

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -10,14 +10,18 @@ class CharactersController < ApplicationController
     if c = Character.find_by_name_and_realm(params[:character][:name].upcase,params[:character][:realm].upcase)
       redirect_to character_path(c)
     else
-      @character = Character.new(params[:character])
-      @character.user = current_user
-      if @character.save
-        flash[:notice] = "Toon added successfully!"
-        redirect_to character_path(@character)
-      else
-        flash[:error] = "There was a problem adding that character"
-        redirect_to account_url
+      begin
+        @character = Character.new(params[:character])
+        @character.user = current_user
+        if @character.save
+          flash[:notice] = "Toon added successfully!"
+          redirect_to character_path(@character)
+        else
+          flash[:error] = "There was a problem adding that character"
+          redirect_to account_url
+        end
+      rescue Wowr::Exceptions::CharacterNotFound
+        render "#{RAILS_ROOT}/public/404.html", :status => 404
       end
     end
   end
--- test/integration/characters_controller_test.rb
+++ test/integration/characters_controller_test.rb
@@ -1,11 +1,12 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class CharactersControllerTest < ActionController::TestCase
+  setup :activate_authlogic
+
   context "post create" do
     should "lookup character if it doesn't exist and redirect show" do
       assert_difference "Item.count", 19 do
         assert_difference "Character.count" do
-          activate_authlogic
           Factory(:user)
           post :create, :character => {:name => "Merb", :realm => "Baelgun"}
           assert_redirected_to character_path(Character.last)
@@ -13,10 +14,11 @@ class CharactersControllerTest < ActionController::TestCase
       end
     end
 
-    should_eventually "display no such character page if we cannot find the character" do
+    should "display no such character page if we cannot find the character" do
+      Factory(:user)
       post :create, :character => {:name => "zzzzzzzzzzerb", :realm => "Thunderlord"}
-      assert_redirected_to no_such_character_home_path(:name => "Zzzzzzzzzzerb", :realm => "Thunderlord")
+      assert_template "#{RAILS_ROOT}/public/404.html"
+      assert_response 404
     end
   end
-
 end
