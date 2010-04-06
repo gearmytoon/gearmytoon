b3e09f99577419301006d44ed16e7ac7d0e7473e
Nolan Evans <nolanevans@nolan-evanss-macbook-pro.local>
2010-04-05 23:30:13 -0700

    fixing the item import

--- lib/max_dps_importer.rb
+++ lib/max_dps_importer.rb
@@ -1,22 +1,22 @@
 class MaxDpsImporter
   attr_reader :agent
-  Hunter = {:base_url => "http://www.maxdps.com/hunter/survival.php", :slot_url_template => "http://www.maxdps.com/hunter/survival_read.php?slotID=", :slots => ((1..17).to_a - [13])}
-  Mage = {:base_url => "http://www.maxdps.com/mage/fire.php", :slot_url_template => "http://www.maxdps.com/mage/fire_read.php?slotID=", :slots => (1..14).to_a}
-  Rogue = {:base_url => "http://www.maxdps.com/rogue/combat.php", :slot_url_template => "http://www.maxdps.com/rogue/combat_read.php?slotID=", :slots => ((1..17).to_a - [13, 15])}
-  FuryWarrior = {:base_url => "http://www.maxdps.com/warrior/fury.php", :slot_url_template => "http://www.maxdps.com/warrior/fury_read.php?slotID=", :slots => (1..14).to_a}
-  ProtectionWarrior = {:base_url => "http://www.maxdps.com/warrior/protection.php", :slot_url_template => "http://www.maxdps.com/warrior/protection_read.php?slotID=", :slots => (1..14).to_a}
-  AfflictionWarlock = {:base_url => "http://www.maxdps.com/warlock/affliction.php", :slot_url_template => "http://www.maxdps.com/warlock/affliction_read.php?slotID=", :slots => (1..14).to_a}
-  RetPaladin = {:base_url => "http://www.maxdps.com/paladin/retribution.php", :slot_url_template => "http://www.maxdps.com/paladin/retribution_read.php?slotID=", :slots => (1..14).to_a}
-  HolyPaladin = {:base_url => "http://www.maxdps.com/paladin/holy.php", :slot_url_template => "http://www.maxdps.com/paladin/holy_read.php?slotID=", :slots => (1..14).to_a}
-  ShadowPreist = {:base_url => "http://www.maxdps.com/priest/shadow.php", :slot_url_template => "http://www.maxdps.com/priest/shadow_read.php?slotID=", :slots => (1..14).to_a}
-  HolyPreist = {:base_url => "http://www.maxdps.com/priest/holy.php", :slot_url_template => "http://www.maxdps.com/priest/holy_read.php?slotID=", :slots => (1..14).to_a}
-  EnhancementShaman = {:base_url => "http://www.maxdps.com/shaman/enhancement.php", :slot_url_template => "http://www.maxdps.com/shaman/enhance_read.php?slotID=", :slots => ((1..14).to_a + ['MH', "OH", 22])}
-  ElementalShaman = {:base_url => "http://www.maxdps.com/shaman/elemental.php", :slot_url_template => "http://www.maxdps.com/shaman/ele_read.php?slotID=", :slots => ((1..14).to_a + ['weap', 16, 22])}
-  RestorationShaman = {:base_url => "http://www.maxdps.com/shaman/restoration.php", :slot_url_template => "http://www.maxdps.com/shaman/resto_read.php?slotID=", :slots => ((1..14).to_a + ['weap', 16, 22])}
-  Boomkin = {:base_url => "http://www.maxdps.com/druid/moonkin.php", :slot_url_template => "http://www.maxdps.com/druid/moonkin_read.php?slotID=", :slots => (1..14).to_a}
-  RestorationDruid = {:base_url => "http://www.maxdps.com/druid/restoration.php", :slot_url_template => "http://www.maxdps.com/druid/resto_read.php?slotID=", :slots => (1..14).to_a}
-  FeralDruid = {:base_url => "http://www.maxdps.com/druid/feral.php", :slot_url_template => "http://www.maxdps.com/druid/feral_read.php?slotID=", :slots => (1..14).to_a}
-  UnholyDeathKnight = {:base_url => "http://www.maxdps.com/deathknight/unholy.php", :slot_url_template => "http://www.maxdps.com/deathknight/unholy_read.php?slotID=", :slots => (1..14).to_a}
+  Hunter = {:base_url => "http://www.maxdps.com/hunter/survival.php", :slot_url_template => "http://www.maxdps.com/hunter/survival_read.php?slotID=", :slots => ((1..11).to_a + ['weap','dualweap',15])}
+  Mage = {:base_url => "http://www.maxdps.com/mage/fire.php", :slot_url_template => "http://www.maxdps.com/mage/fire_read.php?slotID=", :slots => (1..11).to_a + ['weap',20,18]}
+  Rogue = {:base_url => "http://www.maxdps.com/rogue/combat.php", :slot_url_template => "http://www.maxdps.com/rogue/combat_read.php?slotID=", :slots => ((1..11).to_a + [17,"MH", "OH"])}
+  FuryWarrior = {:base_url => "http://www.maxdps.com/warrior/fury.php", :slot_url_template => "http://www.maxdps.com/warrior/fury_read.php?slotID=", :slots => (1..11).to_a + [17,"MH", "OH"]}
+  ProtectionWarrior = {:base_url => "http://www.maxdps.com/warrior/protection.php", :slot_url_template => "http://www.maxdps.com/warrior/protection_read.php?slotID=", :slots => (1..11).to_a + [17,'MH',16]}
+  AfflictionWarlock = {:base_url => "http://www.maxdps.com/warlock/affliction.php", :slot_url_template => "http://www.maxdps.com/warlock/affliction_read.php?slotID=", :slots => (1..11).to_a + ['weap',20,18]}
+  RetPaladin = {:base_url => "http://www.maxdps.com/paladin/retribution.php", :slot_url_template => "http://www.maxdps.com/paladin/retribution_read.php?slotID=", :slots => (1..11).to_a + [19,"weap"]}
+  HolyPaladin = {:base_url => "http://www.maxdps.com/paladin/holy.php", :slot_url_template => "http://www.maxdps.com/paladin/holy_read.php?slotID=", :slots => (1..11).to_a + [19,"weap",16]}
+  ShadowPreist = {:base_url => "http://www.maxdps.com/priest/shadow.php", :slot_url_template => "http://www.maxdps.com/priest/shadow_read.php?slotID=", :slots => (1..11).to_a + ["weap",20,18]}
+  HolyPreist = {:base_url => "http://www.maxdps.com/priest/holy.php", :slot_url_template => "http://www.maxdps.com/priest/holy_read.php?slotID=", :slots => (1..11).to_a + ["weap",20,18]}
+  EnhancementShaman = {:base_url => "http://www.maxdps.com/shaman/enhancement.php", :slot_url_template => "http://www.maxdps.com/shaman/enhance_read.php?slotID=", :slots => ((1..11).to_a + ['MH', "OH", 22])}
+  ElementalShaman = {:base_url => "http://www.maxdps.com/shaman/elemental.php", :slot_url_template => "http://www.maxdps.com/shaman/ele_read.php?slotID=", :slots => ((1..11).to_a + ['weap', 16, 22])}
+  RestorationShaman = {:base_url => "http://www.maxdps.com/shaman/restoration.php", :slot_url_template => "http://www.maxdps.com/shaman/resto_read.php?slotID=", :slots => ((1..11).to_a + ['weap', 16, 22])}
+  Boomkin = {:base_url => "http://www.maxdps.com/druid/moonkin.php", :slot_url_template => "http://www.maxdps.com/druid/moonkin_read.php?slotID=", :slots => (1..11).to_a + ["weap",20,23]}
+  RestorationDruid = {:base_url => "http://www.maxdps.com/druid/restoration.php", :slot_url_template => "http://www.maxdps.com/druid/resto_read.php?slotID=", :slots => (1..11).to_a + ["weap",20,23]}
+  FeralDruid = {:base_url => "http://www.maxdps.com/druid/feral.php", :slot_url_template => "http://www.maxdps.com/druid/feral_read.php?slotID=", :slots => (1..11).to_a + [15,23]}
+  UnholyDeathKnight = {:base_url => "http://www.maxdps.com/deathknight/unholy.php", :slot_url_template => "http://www.maxdps.com/deathknight/unholy_read.php?slotID=", :slots => (1..11).to_a + [42,"MH"]}
 
   def self.build_all_lists
     MaxDpsImporter.constants.each do |klass_spec_to_import|
