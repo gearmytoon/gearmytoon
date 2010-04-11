a2d5cda4c9a3d07145a4aa5476cb01bfa7df1192
Nolan Evans <nolanevans@nolan-evanss-macbook-pro-2.local>
2010-04-10 22:16:08 -0700

    added from_honor_points to upgrades controller

--- app/controllers/upgrades_controller.rb
+++ app/controllers/upgrades_controller.rb
@@ -1,22 +1,17 @@
 class UpgradesController < ApplicationController
   def from_frost_emblems
     @character = Character.find(params[:character_id])
-    respond_to do |format|
-      format.html
-    end
   end
   
   def from_triumph_emblems
     @character = Character.find(params[:character_id])
-    respond_to do |format|
-      format.html
-    end
   end
   
   def from_dungeons
     @character = Character.find(params[:character_id])
-    respond_to do |format|
-      format.html
-    end
+  end
+  
+  def from_honor_points
+    @character = Character.find(params[:character_id])
   end
 end
--- test/functional/upgrades_controller_test.rb
+++ test/functional/upgrades_controller_test.rb
@@ -33,4 +33,15 @@ class UpgradesControllerTest < ActionController::TestCase
       assert_select ".upgrade", :count => 4
     end
   end
+  
+  context "get from_honor_points" do
+    should "show all upgrades from honor points" do
+      character = Factory(:character_item, :character => Factory(:a_hunter)).character
+      4.times {Factory(:item_from_honor_points)}
+      Factory(:item_from_emblem_of_frost)
+      get :from_honor_points, :character_id => character.id
+      assert_response :success
+      assert_select ".upgrade", :count => 4
+    end
+  end
 end
