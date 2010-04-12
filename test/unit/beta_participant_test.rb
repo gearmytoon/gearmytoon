9f595733d7a5bb2d13bc650004e83b774490d39f
Adam Anderson <adamandersonis@gmail.com>
2010-04-12 08:13:27 -0700

    Fixing BetaParticipant test after deleting fixture

--- test/unit/beta_participant_test.rb
+++ test/unit/beta_participant_test.rb
@@ -1,6 +1,8 @@
 require 'test/test_helper'
 
 class BetaParticipantTest < ActiveSupport::TestCase
+  setup { BetaParticipant.create(:email => 'foo@bar.com') }
+
   should_validate_presence_of :email
   should_validate_uniqueness_of :email, :case_sensitive => false
 end
