f4eaaeeb100c21795d81175e50579d396a84c68f
Nolan Evans <nolanevans@nolan-evanss-macbook-pro-2.local>
2010-04-15 20:41:50 -0700

    fixing the broken test

--- test/unit/helpers/characters_helper_test.rb
+++ test/unit/helpers/characters_helper_test.rb
@@ -7,7 +7,7 @@ class CharactersHelperTest < ActionView::TestCase
     should "generate a link to wowhead for the item" do
       item = OpenStruct.new(:icon => "http://www.wowarmory.com/some_item_thumb", :item_id => 4864, :name => "Minshina's Skull")
       tag = wowhead_item_image_link(item)
-      assert_equal "<a href=\"http://www.wowhead.com/?item=4864\"><img alt=\"Minshina's Skull\" border=\"0\" src=\"http://www.wowarmory.com/some_item_thumb\" /></a>", tag
+      assert_equal "<a href=\"http://www.wowhead.com/?item=4864\"><img alt=\"Minshina's Skull\" border=\"0\" class=\"item_icon\" src=\"http://www.wowarmory.com/some_item_thumb\" /></a>", tag
     end
   end
   
