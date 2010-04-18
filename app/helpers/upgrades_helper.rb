51b5582f024a690ddd025908d9f50f9139c798fa
Nolan Evans <nolanevans@nolan-evanss-macbook-pro-2.local>
2010-04-18 13:05:55 -0700

    added titles to tabs

--- app/helpers/upgrades_helper.rb
+++ app/helpers/upgrades_helper.rb
@@ -1,7 +1,7 @@
 module UpgradesHelper
-  def tab_link(icon, url)
+  def tab_link(icon, url, title)
     classes = ["tab"]
     classes << "selected" if url == request.request_uri
-    content_tag(:li, link_to(icon, url), :class => classes.join(" "))
+    content_tag(:li, link_to(icon, url, :title => title), :class => classes.join(" "))
   end
 end
