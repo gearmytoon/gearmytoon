28247f800213c759d1ac446133b51827335615e3
Adam Anderson <adamandersonis@gmail.com>
2010-05-20 09:04:29 -0700

    Pointing deploys to EC2 now

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,19 +1,24 @@
 set :application, "project"
-set :domain, "deploy@gearmytoon.com"
-set :deploy_to, "/var/public_html/gearmytoon.com/"
-set :repository, 'git@github.com:nolman/wowcoach.git'
-set :ssh_flags, ['-p 60322']
+set :domain, "deploy@gmt-ec2"
+set :deploy_to, "/var/www/gearmytoon.com/"
+set :repository, 'git@github.com:nolman/gearmytoon.git'
 set :revision, 'master' # git branch to deploy
-set :web_command, 'sudo apache2ctl' # command to start/stop apache
+set :web_command, 'sudo service nginx' # command to start/stop nginx
 
 namespace :vlad do
   desc "deploy the app"
   task :deploy => %w[
     vlad:update
     vlad:bundle:install
+    vlad:create
     vlad:migrate
     vlad:start
   ]
+
+  desc "create database"
+  remote_task :create do
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} rake db:create"
+  end
 
   desc "import a text file via wow armory on production"
   remote_task :import_a_items_text_file do
@@ -35,7 +40,7 @@ namespace :vlad do
   namespace :bundle do
     desc "install gems"
     remote_task :install do
-      run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle install"
+      run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle install --without test"
     end
   end
 end
