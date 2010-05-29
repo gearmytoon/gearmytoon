7dbe20f396bc54a7573ad15dab719f9dc982d5a5
Nolan Evans <nolanevans@nolan-evanss-macbook-pro-2.local>
2010-05-29 15:25:12 -0700

    i don't know why and i don't like it but rails doesn't let us put config.middlewear.use(Rach::Cache in development.rb, it sucks like this but just getting it so we can do script server again

--- config/environment.rb
+++ config/environment.rb
@@ -8,6 +8,11 @@ end
 Rails::Initializer.run do |config|
   require_all_files_in("#{RAILS_ROOT}/lib/extensions")
 
+  if RAILS_ENV == "development"
+    require 'rack/cache'
+    config.middleware.use(Rack::Cache, :verbose => true, :metastore   => "file:#{RAILS_ROOT}/cache/rack/meta", :entitystore => "file:#{RAILS_ROOT}/cache/rack/body") 
+  end
+
   config.time_zone = 'UTC'
   config.after_initialize do
     RPXNow.api_key = '65d9b768ba9f8c96a1d93691c53df7e59c738599'
