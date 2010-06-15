50e888fe025717bdc3504bf38bba6e92c2201788
Nolan Evans <nolanevans@nolan-evanss-macbook-pro-2.local>
2010-06-15 11:03:20 -0700

    handing adding of nil hashes gracefully

--- lib/extensions/hash.rb
+++ lib/extensions/hash.rb
@@ -1,23 +1,22 @@
 class Hash
   def subtract_values(other_hash)
-    returning({}) do |new_hash|
-      self.each do |key, value|
-        other_hash_value = other_hash.has_key?(key) ? other_hash[key] : 0.0
-        new_hash[key] = value - other_hash_value
-      end
-      values_unique_to_other_hash = other_hash.except(*self.keys)
-      values_unique_to_other_hash.each {|key, value| new_hash[key] = -value }
-    end
+    combine_hash(other_hash, true)
   end
   
   def add_values(other_hash)
+    combine_hash(other_hash, false)
+  end
+  
+  def combine_hash(other_hash, subtraction)
+    operator = subtraction ? -1.0 : 1.0
+    return self if other_hash.nil?
     returning({}) do |new_hash|
       self.each do |key, value|
         other_hash_value = other_hash.has_key?(key) ? other_hash[key] : 0.0
-        new_hash[key] = value + other_hash_value
+        new_hash[key] = value + (other_hash_value * operator)
       end
       values_unique_to_other_hash = other_hash.except(*self.keys)
-      values_unique_to_other_hash.each {|key, value| new_hash[key] = value }
+      values_unique_to_other_hash.each {|key, value| new_hash[key] = (value * operator) }
     end
   end
 end
---
+++ test/unit/hash_test.rb
@@ -0,0 +1,42 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class HashTest < ActiveSupport::TestCase
+  context "add_values" do
+    should "know how to add hashes with the same keys" do
+      assert_equal({:agility => 100}, {:agility => 45}.add_values({:agility => 55}))
+    end
+
+    should "know how to add hashes with different keys" do
+      assert_equal({:agility => 1, :stamina => 1}, {:agility => 1}.add_values({:stamina => 1}))
+    end
+
+    should "know how to add hashes if only one has a extra key" do
+      assert_equal({:agility => 1, :stamina => 2}, {:stamina => 1}.add_values({:stamina => 1, :agility => 1}))
+      assert_equal({:agility => 1, :stamina => 2}, {:agility => 1, :stamina => 1}.add_values({:stamina => 1}))
+    end
+
+    should "handle adding to nil hashes gracefully" do
+      assert_equal({:stamina => 1}, {:stamina => 1}.add_values(nil))
+    end
+  end
+  
+  context "subtract_values" do
+    should "know how to subtract hashes with the same keys" do
+      assert_equal({:agility => -10}, {:agility => 45}.subtract_values({:agility => 55}))
+    end
+
+    should "know how to add hashes with different keys" do
+      assert_equal({:agility => 1, :stamina => -1}, {:agility => 1}.subtract_values({:stamina => 1}))
+    end
+
+    should "know how to add hashes if only one has a extra key" do
+      assert_equal({:agility => -1, :stamina => 0}, {:stamina => 1}.subtract_values({:stamina => 1, :agility => 1}))
+      assert_equal({:agility => 1, :stamina => 0}, {:agility => 1, :stamina => 1}.subtract_values({:stamina => 1}))
+    end
+
+    should "handle adding to nil hashes gracefully" do
+      assert_equal({:stamina => 1}, {:stamina => 1}.subtract_values(nil))
+    end
+  end
+  
+end
