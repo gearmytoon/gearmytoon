d1b6541561495bcef7b5abb7565574e96444081b
Adam Anderson <adamandersonis@gmail.com>
2010-07-09 22:13:17 -0700

    Restructure PaymentsHelperTest. Still broken, but much easier to parse :]

--- app/helpers/payments_helper.rb
+++ app/helpers/payments_helper.rb
@@ -1,38 +1,38 @@
 module PaymentsHelper
-  
+
   def subscription_simple_pay_form(reference_id)
-    params = basic_params(reference_id,"recurringFrequency" => "1 month", "description" => "Gear My Toon Subscription", "amount" => "USD 3")
+    params = basic_params(reference_id, "recurringFrequency" => "1 month", "description" => "Gear My Toon Subscription", "amount" => "USD 3")
     content_tag :form, :action => "https://#{params[:host]}#{params[:uri]}", :method => params[:verb] do
-      fields = params[:parameters].map{ |key, value| hidden_field_tag(key, value)}
+      fields = params[:parameters].map {|key, value| hidden_field_tag(key, value)}
       fields << hidden_field_tag('signature', SignatureUtils.sign_parameters(params))
-      fields << "<input type=\"image\" src=\"http://g-ecx.images-amazon.com/images/G/01/asp/beige_small_subscribe_withlogo_darkbg.gif\" border=\"0\" />"
+      fields << image_submit_tag("http://g-ecx.images-amazon.com/images/G/01/asp/beige_small_subscribe_withlogo_darkbg.gif", :border => "0")
       fields
     end
   end
-  
+
   def standard_simple_pay_form(reference_id)
     params = basic_params(reference_id,"isDonationWidget" => "0", "description" => "Gear My Toon Trial Account", "amount"=> "USD 1")
     content_tag :form, :action => "https://#{params[:host]}#{params[:uri]}", :method => params[:verb] do
-      fields = params[:parameters].map{ |key, value| hidden_field_tag(key, value)}
+      fields = params[:parameters].map {|key, value| hidden_field_tag(key, value)}
       fields << hidden_field_tag('signature', SignatureUtils.sign_parameters(params))
-      fields << "<input type=\"image\" src=\"http://g-ecx.images-amazon.com/images/G/01/asp/beige_small_paynow_withlogo_darkbg.gif\" border=\"0\"/>"
+      fields << image_submit_tag("http://g-ecx.images-amazon.com/images/G/01/asp/beige_small_paynow_withlogo_darkbg.gif", :border => "0")
       fields
     end
   end
-  
+
   def generate_reference_id(user_id)
     "#{user_id}-#{Time.now.to_i}"
   end
-  
+
   def basic_params(reference_id, payment_params)
-    #TODO if not test env:: ADD "ipnUrl"=>notify_payment_payment_url, 
+    #TODO if not test env:: ADD "ipnUrl"=>notify_payment_payment_url,
     payment_params.merge!("ipnUrl" => notify_payment_payment_url) if(RAILS_ENV=="production")
     host = RAILS_ENV=="production" ? "authorize.payments.amazon.com" : "authorize.payments-sandbox.amazon.com"
-    params = {:host=>host, :verb=>"POST", :algorithm=>"HmacSHA1", 
-      :uri=>"/pba/paypipeline", 
+    params = {:host=>host, :verb=>"POST", :algorithm=>"HmacSHA1",
+      :uri=>"/pba/paypipeline",
       :parameters=>{"returnUrl"=>receipt_payment_url, "collectShippingAddress" => "0", "referenceId" => reference_id,
-        "cobrandingStyle"=>"logo", "signatureVersion"=>"2", "abandonUrl"=>payment_url, "immediateReturn"=>"1", 
-        "accessKey"=>"AKIAJDSU5CTXKVEMH6OA", "processImmediate"=>"1", "signatureMethod"=>"HmacSHA1"}.merge(payment_params), 
+        "cobrandingStyle"=>"logo", "signatureVersion"=>"2", "abandonUrl"=>payment_url, "immediateReturn"=>"1",
+        "accessKey"=>"AKIAJDSU5CTXKVEMH6OA", "processImmediate"=>"1", "signatureMethod"=>"HmacSHA1"}.merge(payment_params),
       :aws_secret_key=>"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"}
   end
 end
--- test/unit/helpers/payments_helper_test.rb
+++ test/unit/helpers/payments_helper_test.rb
@@ -2,15 +2,54 @@ require File.dirname(__FILE__) + '/../../test_helper'
 
 class PaymentsHelperTest < ActionView::TestCase
 
-  #TODO: improve asserts
   should "build a standard pay form" do
-    expected = '<form action="https://authorize.payments-sandbox.amazon.com/pba/paypipeline" method="POST"><input id="isDonationWidget" name="isDonationWidget" type="hidden" value="0" /><input id="returnUrl" name="returnUrl" type="hidden" value="http://test.host/payment/receipt" /><input id="cobrandingStyle" name="cobrandingStyle" type="hidden" value="logo" /><input id="signatureVersion" name="signatureVersion" type="hidden" value="2" /><input id="abandonUrl" name="abandonUrl" type="hidden" value="http://test.host/payment" /><input id="immediateReturn" name="immediateReturn" type="hidden" value="1" /><input id="accessKey" name="accessKey" type="hidden" value="AKIAJDSU5CTXKVEMH6OA" /><input id="description" name="description" type="hidden" value="Gear My Toon Trial Account" /><input id="amount" name="amount" type="hidden" value="USD 1" /><input id="collectShippingAddress" name="collectShippingAddress" type="hidden" value="0" /><input id="processImmediate" name="processImmediate" type="hidden" value="1" /><input id="signatureMethod" name="signatureMethod" type="hidden" value="HmacSHA1" /><input id="referenceId" name="referenceId" type="hidden" value="1" /><input id="signature" name="signature" type="hidden" value="/T/Rty4ra/70356YnKvYbZhPkbo=" /><input type="image" src="http://g-ecx.images-amazon.com/images/G/01/asp/beige_small_paynow_withlogo_darkbg.gif" border="0"/></form>'
-    assert_equal expected, standard_simple_pay_form(1)
+    @output_buffer = ""
+    @request = ActionController::TestRequest.new
+    @response = ActionController::TestResponse.new
+    @response.body = standard_simple_pay_form(1)
+
+    assert_select('form[action=https://authorize.payments-sandbox.amazon.com/pba/paypipeline][method=POST]') do
+      assert_select 'input[id=recurringFrequency][name=recurringFrequency][type=hidden][value=1 month]'
+      assert_select 'input[id=returnUrl][name=returnUrl][type=hidden][value=http://test.host/payment/receipt]'
+      assert_select 'input[id=cobrandingStyle][name=cobrandingStyle][type=hidden][value=logo]'
+      assert_select 'input[id=signatureVersion][name=signatureVersion][type=hidden][value=2]'
+      assert_select 'input[id=abandonUrl][name=abandonUrl][type=hidden][value=http://test.host/payment]'
+      assert_select 'input[id=immediateReturn][name=immediateReturn][type=hidden][value=1]'
+      assert_select 'input[id=accessKey][name=accessKey][type=hidden][value=AKIAJDSU5CTXKVEMH6OA]'
+      assert_select 'input[id=description][name=description][type=hidden][value=Gear My Toon Subscription]'
+      assert_select 'input[id=amount][name=amount][type=hidden][value=USD 3]'
+      assert_select 'input[id=collectShippingAddress][name=collectShippingAddress][type=hidden][value=0]'
+      assert_select 'input[id=processImmediate][name=processImmediate][type=hidden][value=1]'
+      assert_select 'input[id=signatureMethod][name=signatureMethod][type=hidden][value=HmacSHA1]'
+      assert_select 'input[id=referenceId][name=referenceId][type=hidden][value=2]'
+      assert_select 'input[id=signature][name=signature][type=hidden][value=UYChovQgo98YhRyME62soV+GvD8=]'
+      assert_select 'input[type=image][src=http://g-ecx.images-amazon.com/images/G/01/asp/beige_small_subscribe_withlogo_darkbg.gif][border=0]'
+    end
   end
 
   should "build a subscription pay form" do
-    expected = '<form action="https://authorize.payments-sandbox.amazon.com/pba/paypipeline" method="POST"><input id="recurringFrequency" name="recurringFrequency" type="hidden" value="1 month" /><input id="returnUrl" name="returnUrl" type="hidden" value="http://test.host/payment/receipt" /><input id="cobrandingStyle" name="cobrandingStyle" type="hidden" value="logo" /><input id="signatureVersion" name="signatureVersion" type="hidden" value="2" /><input id="abandonUrl" name="abandonUrl" type="hidden" value="http://test.host/payment" /><input id="immediateReturn" name="immediateReturn" type="hidden" value="1" /><input id="accessKey" name="accessKey" type="hidden" value="AKIAJDSU5CTXKVEMH6OA" /><input id="description" name="description" type="hidden" value="Gear My Toon Subscription" /><input id="amount" name="amount" type="hidden" value="USD 3" /><input id="collectShippingAddress" name="collectShippingAddress" type="hidden" value="0" /><input id="processImmediate" name="processImmediate" type="hidden" value="1" /><input id="signatureMethod" name="signatureMethod" type="hidden" value="HmacSHA1" /><input id="referenceId" name="referenceId" type="hidden" value="2" /><input id="signature" name="signature" type="hidden" value="UYChovQgo98YhRyME62soV+GvD8=" /><input type="image" src="http://g-ecx.images-amazon.com/images/G/01/asp/beige_small_subscribe_withlogo_darkbg.gif" border="0" /></form>'
-    assert_equal expected, subscription_simple_pay_form(2)
+    @output_buffer = ""
+    @request = ActionController::TestRequest.new
+    @response = ActionController::TestResponse.new
+    @response.body = subscription_simple_pay_form(2)
+
+    assert_select('form[action=https://authorize.payments-sandbox.amazon.com/pba/paypipeline][method=POST]') do
+      assert_select 'input[id=recurringFrequency][name=recurringFrequency][type=hidden][value=1 month]'
+      assert_select 'input[id=returnUrl][name=returnUrl][type=hidden][value=http://test.host/payment/receipt]'
+      assert_select 'input[id=cobrandingStyle][name=cobrandingStyle][type=hidden][value=logo]'
+      assert_select 'input[id=signatureVersion][name=signatureVersion][type=hidden][value=2]'
+      assert_select 'input[id=abandonUrl][name=abandonUrl][type=hidden][value=http://test.host/payment]'
+      assert_select 'input[id=immediateReturn][name=immediateReturn][type=hidden][value=1]'
+      assert_select 'input[id=accessKey][name=accessKey][type=hidden][value=AKIAJDSU5CTXKVEMH6OA]'
+      assert_select 'input[id=description][name=description][type=hidden][value=Gear My Toon Subscription]'
+      assert_select 'input[id=amount][name=amount][type=hidden][value=USD 3]'
+      assert_select 'input[id=collectShippingAddress][name=collectShippingAddress][type=hidden][value=0]'
+      assert_select 'input[id=processImmediate][name=processImmediate][type=hidden][value=1]'
+      assert_select 'input[id=signatureMethod][name=signatureMethod][type=hidden][value=HmacSHA1]'
+      assert_select 'input[id=referenceId][name=referenceId][type=hidden][value=2]'
+      assert_select 'input[id=signature][name=signature][type=hidden][value=UYChovQgo98YhRyME62soV+GvD8=]'
+      assert_select 'input[type=image][src=http://g-ecx.images-amazon.com/images/G/01/asp/beige_small_subscribe_withlogo_darkbg.gif][border=0]'
+    end
   end
 end
 
