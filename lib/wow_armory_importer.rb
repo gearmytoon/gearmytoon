9b8c1f437799da88841d5f57cfa7b6d895a14575
Nolan Evans <none@none.com>
2010-07-30 22:46:52 -0700

    upgraded mechanize and nokogiri, removed hpricot in favor of nokogiri

--- lib/wow_armory_importer.rb
+++ lib/wow_armory_importer.rb
@@ -2,6 +2,8 @@ class WowArmoryImporter
 
   def initialize(wowarmory_item_id)
     @wowarmory_item_id = wowarmory_item_id
+    @agent = Mechanize.new
+    @agent.user_agent = 'Mozilla/5.0 Gecko/20070219 Firefox/2.0.0.2'
   end
   
   def tooltip
@@ -13,17 +15,7 @@ class WowArmoryImporter
   end
 
   def get_xml(url)
-    response = http_request(url)
-    Hpricot.XML(response)
-  end
-
-  def http_request(url)
-    req = Net::HTTP::Get.new(url)
-    req["user-agent"] = 'Mozilla/5.0 Gecko/20070219 Firefox/2.0.0.2' #ensure xml is returned
-    uri = URI.parse(URI.escape(url))
-    http = Net::HTTP.new(uri.host, uri.port)
-    res = http.request req
-    res.body
+    Nokogiri::XML(@agent.get(url).body)
   end
 
   def item_info_url(wowarmory_item_id)
