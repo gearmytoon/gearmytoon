a61dda114b22f5a58b28073d52a8a115e5358184
Nolan Evans <nolanevans@Nolan-Evanss-MacBook-Pro-2.local>
2010-08-29 18:11:24 -0700

    extracted areas factories

--- test/factories/factories.rb
+++ test/factories/factories.rb
@@ -45,34 +45,6 @@ Factory.define(:quest) do |model|
   model.association :area, :factory => :dungeon
 end
 
-Factory.define(:dungeon, :class => :area) do |model|
-  model.name "Super Fun Unicorn Land"
-  model.wowarmory_area_id Area::DUNGEONS.first
-  model.players 5
-end
-
-Factory.define(:heroic_dungeon, :class => :area) do |model|
-  model.name "Super Fun Unicorn Land"
-  model.wowarmory_area_id Area::DUNGEONS.first
-  model.difficulty Area::HEROIC
-  model.players 5
-end
-
-Factory.define(:raid_25, :class => :area) do |model|
-  model.name "Super DUPER Fun Unicorn Land (25)"
-  model.wowarmory_area_id Area::RAIDS.first
-  model.players 25
-end
-
-Factory.define(:raid_10, :class => :area) do |model|
-  model.name "Super DUPER Fun Unicorn Land (10)"
-  model.wowarmory_area_id Area::RAIDS.first
-  model.players 10
-end
-
-Factory.define(:raid, :parent => :raid_10) do |model|
-end
-
 Factory.define(:user_character) do |model|
   model.association :subscriber, :factory => :user
   model.association :character, :factory => :unpaid_character
