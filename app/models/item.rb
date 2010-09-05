9d4f4425cd33c9c32156ebd9ec66f3fbccb36da2
Nolan Evans <none@none.com>
2010-09-05 09:43:51 -0700

    moved item summarizing logic into item and removed item summarizer class

--- app/models/item.rb
+++ app/models/item.rb
@@ -126,4 +126,21 @@ class Item < ActiveRecord::Base
       "#{trigger_method}: #{spell_effect[:description]}"
     end
   end
+  
+  def summarize
+    self.item_popularities.destroy_all
+    all_specs = Spec.all_played_specs
+    all_specs.each do |spec|
+      used_by_count = self.character_items.used_by(spec).count
+      next if used_by_count == 0
+      total_used = self.character_items.count
+      percentage = (used_by_count.to_f / total_used)*100
+      gmt_scores = Character.all(:joins => :character_items, :conditions => ['character_items.item_id = ?', self.id], :select => :gmt_score).map(&:gmt_score)
+      total_gmt_score = gmt_scores.sum
+      average_gmt_score = total_gmt_score / used_by_count
+      self.item_popularities.create!(:spec => spec, :percentage => percentage, :average_gmt_score => average_gmt_score)
+    end
+    
+  end
+
 end
